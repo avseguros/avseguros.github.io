35924635fb5828518e5978e72cdbcbe5eee1ab43
Nicolas Vargas Ortega <nvortega92@gmail.com>
2021-04-28 19:27:46 +0200

    Avoid show errro and change status of button when send

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -55,9 +55,7 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Sorry " +
-                                firstName +
-                                ", it seems that my mail server is not responding. Please try again later!"
+                            ""
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
